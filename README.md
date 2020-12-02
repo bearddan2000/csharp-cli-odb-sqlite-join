c255365e9a8dd2bef3867041d2a34f5083bbaea6
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2020-12-01 23:10:23 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# csharp-cli-odb-sqlite-join
+
+## Description
+A c# running local sqlite database.
+The query is a JOIN of two tables.
+
+## Tech stack
+- bash
+- c#
+  - Framework 4.7
+- sqlite3
+- mono 6.8
+
+## Docker stack
+- docker-cli
+- ubuntu:20.04
+
+## Requirements
+Docker desktop must be installed and the application
+being called must be linux compatiple.
+
+## To run
+`sudo ./install.sh -u`
+
+## To stop (optional)
+`sudo ./install.sh -d`
+
+## To see help
+`sudo ./install.sh -h`
---
+++ bin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+namespace CSharpConsol
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const string connectionString = "URI=file:person.db";
+            IDbConnection dbcon = new SqliteConnection(connectionString);
+            dbcon.Open();
+            using (IDbCommand dbcmd = dbcon.CreateCommand())
+            {
+                string sql = "SELECT p.name, a.edad "; 
+                sql += "FROM person AS p ";
+                sql += "JOIN age AS a ON a.ageId = p.ageId";
+                dbcmd.CommandText = sql;
+                using (IDataReader reader = dbcmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader.GetString(0);
+                        int age = reader.GetInt32(1);
+                        Console.WriteLine("Name: {0}, Age: {1}", name, age);
+                    }
+                }
+            }
+            dbcon.Close();
+        }
+    }
+}
